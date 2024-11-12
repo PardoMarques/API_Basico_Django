111d60eb927629fa47ec955a1031fd4fe7dbfda1
Caio Marques <caio_asm@hotmail.com>
2024-11-12 02:01:00 -0300

    Commit 3: Definição do Model Car

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    make = models.CharField("Marca", max_length=50)
+    model = models.CharField("Modelo", max_length=50)
+    year = models.PositiveIntegerField("Ano")
+    price = models.DecimalField("Preço", max_digits=10, decimal_places=2)
+    description = models.TextField("Descrição", blank=True)
+    created_at = models.DateTimeField("Data de Criação", auto_now_add=True)
+    updated_at = models.DateTimeField("Data de Atualização", auto_now=True)
+
+    def __str__(self):
+        return f"{self.make} {self.model} ({self.year})"
